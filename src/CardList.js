5d1316957aaa0d3402319efcb5ab79d584526379
mohab <mohab-elsayed@pm.me>
2022-11-20 20:58:26 +0200

    create cards component

---
+++ src/CardList.js
@@ -0,0 +1,13 @@
+import Card from './Card';
+
+const CardList = ({ robots }) => {
+  return (
+    <>
+      {robots.map((user) => (
+        <Card key={user.id} id={user.id} name={user.name} email={user.email} />
+      ))}
+    </>
+  );
+};
+
+export default CardList;
